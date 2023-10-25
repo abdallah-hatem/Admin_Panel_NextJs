81a6c7905aa7762a01d275ea7a028154ad2c26db
abdallah-hatem <ahkortam@gmail.com>
2023-10-26 02:38:40 +0300

    added auth context

--- context/auth.jsx
+++ context/auth.jsx
@@ -1,11 +1,18 @@
 "use client";
-import { createContext, useContext, useState } from "react";
+import Cookies from "js-cookie";
+import { createContext, useContext, useEffect, useState } from "react";
 // Creating the user context
 const AuthContext = createContext();
 
 // Making the function which will wrap the whole app using Context Provider
 export default function AuthProvider({ children }) {
   const [isAuth, setIsAuth] = useState(false);
+
+  const token = !(Cookies.get("jwt") === undefined);
+  useEffect(() => {
+    setIsAuth(token);
+  }, [token]);
+
   return (
     <AuthContext.Provider value={{ isAuth, setIsAuth }}>
       {children}
